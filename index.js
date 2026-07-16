34214d6358be595d64a33f9942472d3c42342f46
mazen-albattat <mazen.battat45@gmail.com>
2026-07-17 00:34:29 +0300

    Add edit and delete features for book notes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function getISBNandCover(name) {
     
 }
 app.get('/', async (req, res) => {
-    const data = await db.query("SELECT * FROM books");
+    const data = await db.query("SELECT * FROM books ORDER BY note_date DESC");
     const books = data.rows;
     console.log("BOOKS: ", books);
     
@@ -50,11 +50,11 @@ app.get('/', async (req, res) => {
 })
 
 app.get("/add", (req, res) => {
-    res.render("add.ejs");
+    res.render("add.ejs", {book: null, action: "Add"});
 })
 
 app.post("/add", async (req, res) => {
-    let { name, isbn, description, notes, rating  } = req.body;
+    let { name, isbn, description, notes, rating } = req.body;
     console.log("BODY: ", req.body);
     let author, url;
     try {
@@ -63,7 +63,7 @@ app.post("/add", async (req, res) => {
             const response = await axios.get(`https://openlibrary.org/search.json?title=${name.toLowerCase()}&fields=author_name`);
             author = response.data.docs[0].author_name[0];
         } else {
-            ({url, author, isbn} = await getISBNandCover(name));
+            ({ url, author, isbn } = await getISBNandCover(name));
 
         }
 
@@ -72,8 +72,15 @@ app.post("/add", async (req, res) => {
         
         console.log("DATE: ", date);
         console.log("ISBN: ", isbn, '\n', "author: ", author);
+        let temp = name.split(" ");
+        let upperName = "";
+        for (let word of temp) {
+            upperName += `${word[0].toUpperCase()}${word.substr(1, word.length - 1)}`;
+            upperName += " ";
+        }
+        name = upperName;
         const newNote = await db.query("INSERT INTO books (title, author, isbn,  notes,  note_date, rating, cover, description) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)",
-            [name, author, isbn,notes , date ,rating, url, description  ]
+            [name, author, isbn, notes, date, rating, url, description]
         )
         res.redirect("/");
     } catch (err) {
@@ -82,10 +89,79 @@ app.post("/add", async (req, res) => {
     }
   
     
+});
+app.post("/view/:id", async (req, res) => {
+    const id = req.body.id;
+    console.log("BOOK ID: ", id);
+    const data = await db.query
+        ("SELECT * FROM books WHERE id = $1", [id]);
+    const book = data.rows[0];
+    console.log("BOOK: ", book);
+    res.render("view.ejs", { book: book });
 })
 
+app.get("/view/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log("BOOK ID: ", id);
+    const data = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+    const book = data.rows[0];
+    console.log("BOOK: ", book);
+    res.render("view.ejs", { book: book });
+})
+
+app.post("/sort", async (req, res) => {
+    try {
+            if (req.body.sort === "date") {
+        res.redirect("/");
+    }
+    else if(req.body.sort === "rate"){
+        const result = await db.query("SELECT * FROM books ORDER BY rating DESC");
+                const books = result.rows;
+                console.log("SORT BOOKS: ", books);
+        res.render("index.ejs", { books: books });
+    }
+    } catch (err) {
+        console.log("ERROR: ", err.message);
+    }
+
+})
+app.post("/edit", async (req, res) => {
+    
+    try {
+        const id = parseInt(req.body.edit);
+        const result = await db.query("SELECT title, isbn, description, notes, rating, id FROM books WHERE id = $1;", [id]);
+        const book = result.rows[0];
+
+        console.log("result: ",book);
 
 
+
+        res.render("add.ejs", { book: book, action: "Update" });
+
+    } catch (err) {
+        console.log("Error: ", err.message);
+    }
+})
+
+app.post("/Update", async (req, res) => {
+
+    const { description, notes, rating, update} = req.body;
+    const d = new Date();
+    console.log("body: ", req.body);
+    console.log("UPDATE: ", update);
+    console.log("Parsed: ", parseInt(update));
+    await db.query(`UPDATE books SET  description = $1, notes = $2, rating = $3, note_date = $4 WHERE id = $5 `,
+        [description, notes, rating, d.toISOString(), parseInt(update)]);
+    
+    res.redirect(`/view/${update}`);
+})
+
+app.post("/delete", async (req, res) => {
+    const id = parseInt(req.body.id);
+    console.log("ID: ", id);
+    await db.query("DELETE FROM books WHERE id = $1", [id]);
+    res.redirect("/");
+})
 
 
 app.listen(port, () => {
