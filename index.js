f8dc9fd31ff47bed3a2615509a4f1b298be2aa86
mazen-albattat <mazen.battat45@gmail.com>
2026-07-18 23:56:01 +0300

    Fix some bugs and add style changes

--- index.js
+++ index.js
@@ -24,15 +24,20 @@ app.use(express.static("public"));
 
 
 db.connect();
-
 async function getISBNandCover(name) {
     const isbn_url = `https://openlibrary.org/search.json?title=${name.toLowerCase()}&fields=author_name,isbn`;
         try {
             const result = await axios.get(isbn_url);
-            const isbn = result.data.docs[0].isbn[1];
-            const author = result.data.docs[0].author_name[0];
-            const cover_url = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`
-            return { url: cover_url, author: author, isbn: isbn };
+            console.log("RESULT: ", result);
+            if (result.data.docs.length !== 0) {
+                const isbn = result.data.docs[0].isbn[1];
+                const author = result.data.docs[0].author_name[0];
+                const cover_url = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`
+                return { url: cover_url, author: author, isbn: isbn };                
+            } else {
+                return null;
+            }
+
         } catch (err) {
             console.log("Error making request: ", err);
 
@@ -42,11 +47,23 @@ async function getISBNandCover(name) {
     
 }
 app.get('/', async (req, res) => {
-    const data = await db.query("SELECT * FROM books ORDER BY note_date DESC");
-    const books = data.rows;
-    console.log("BOOKS: ", books);
-    
-    res.render("index.ejs", { books: books });
+    try {
+        const sort = req.query.sort;
+        if (!sort || sort === 'date') {
+            const data = await db.query("SELECT * FROM books ORDER BY note_date DESC");
+            const books = data.rows;        
+            res.render("index.ejs", { books: books });
+        } else if (sort === 'rating') {
+            const data = await db.query("SELECT * FROM books ORDER BY rating DESC");
+            const books = data.rows;
+            res.render("index.ejs", { books: books });
+
+        }
+
+
+    } catch (error) {
+        console.log("Error: ", error.message);
+    }
 })
 
 app.get("/add", (req, res) => {
@@ -54,35 +71,49 @@ app.get("/add", (req, res) => {
 })
 
 app.post("/add", async (req, res) => {
-    let { name, isbn, description, notes, rating } = req.body;
-    console.log("BODY: ", req.body);
+    let { title, isbn, description, notes, rating } = req.body;
     let author, url;
+    const book = { title: title, description: description, notes: notes, rating: rating };
+    console.log(book);
     try {
-        if (isbn) {
-            url = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`;
-            const response = await axios.get(`https://openlibrary.org/search.json?title=${name.toLowerCase()}&fields=author_name`);
-            author = response.data.docs[0].author_name[0];
+        if (!(title && description && notes && rating)) {
+            return res.render("add.ejs", {book: book, action:"Add", error: "Please fill in all required fields before submitting."})
         } else {
-            ({ url, author, isbn } = await getISBNandCover(name));
+            
+            if (isbn) {
+                url = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`;
+                const response = await axios.get(`https://openlibrary.org/search.json?title=${title.toLowerCase()}&fields=author_name`);
+                console.log("RESPONSE: ", response);
+                author = response.data.docs[0].author_name[0];
+            } else {
+                const result = await getISBNandCover(title);
+                console.log("RESULT: ", result);
+                if (!result) {
+                    return res.render("add.ejs", { book: book, action: "Add", error: "Please enter a valid book name" });
+                }
+                else {
+                    author = result.author;
+                    url = result.url;
+                    isbn = result.isbn;
+                }
 
+            }
+            
+            let d = new Date();
+            let date = d.toLocaleDateString('en-CA');
+            
+            let temp = title.split(" ");
+            let upperName = "";
+            for (let word of temp) {
+                upperName += `${word[0].toUpperCase()}${word.substr(1, word.length - 1)}`;
+                upperName += " ";
+            }
+            title = upperName;
+            const newNote = await db.query("INSERT INTO books (title, author, isbn,  notes,  note_date, rating, cover, description) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)",
+                [title, author, isbn, notes, date, rating, url, description]
+            )
+            res.redirect("/");
         }
-
-        let d = new Date();
-        let date = d.toISOString();
-        
-        console.log("DATE: ", date);
-        console.log("ISBN: ", isbn, '\n', "author: ", author);
-        let temp = name.split(" ");
-        let upperName = "";
-        for (let word of temp) {
-            upperName += `${word[0].toUpperCase()}${word.substr(1, word.length - 1)}`;
-            upperName += " ";
-        }
-        name = upperName;
-        const newNote = await db.query("INSERT INTO books (title, author, isbn,  notes,  note_date, rating, cover, description) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)",
-            [name, author, isbn, notes, date, rating, url, description]
-        )
-        res.redirect("/");
     } catch (err) {
         console.log("Failed to fetch data: ", err.message);
         
@@ -91,51 +122,38 @@ app.post("/add", async (req, res) => {
     
 });
 app.post("/view/:id", async (req, res) => {
-    const id = req.body.id;
-    console.log("BOOK ID: ", id);
+    try {
+            const id = req.body.id;
     const data = await db.query
         ("SELECT * FROM books WHERE id = $1", [id]);
     const book = data.rows[0];
-    console.log("BOOK: ", book);
     res.render("view.ejs", { book: book });
+    } catch (error) {
+        console.log("Failed to fetch data: ", error.message);
+
+    }
+
 })
 
 app.get("/view/:id", async (req, res) => {
-    const id = req.params.id;
-    console.log("BOOK ID: ", id);
-    const data = await db.query("SELECT * FROM books WHERE id = $1", [id]);
-    const book = data.rows[0];
-    console.log("BOOK: ", book);
-    res.render("view.ejs", { book: book });
-})
-
-app.post("/sort", async (req, res) => {
     try {
-            if (req.body.sort === "date") {
-        res.redirect("/");
-    }
-    else if(req.body.sort === "rate"){
-        const result = await db.query("SELECT * FROM books ORDER BY rating DESC");
-                const books = result.rows;
-                console.log("SORT BOOKS: ", books);
-        res.render("index.ejs", { books: books });
-    }
-    } catch (err) {
-        console.log("ERROR: ", err.message);
+        const id = req.params.id;
+        const data = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+        const book = data.rows[0];
+        res.render("view.ejs", { book: book });
+    } catch (error) {
+        
     }
 
 })
+
+
 app.post("/edit", async (req, res) => {
     
     try {
         const id = parseInt(req.body.edit);
         const result = await db.query("SELECT title, isbn, description, notes, rating, id FROM books WHERE id = $1;", [id]);
         const book = result.rows[0];
-
-        console.log("result: ",book);
-
-
-
         res.render("add.ejs", { book: book, action: "Update" });
 
     } catch (err) {
@@ -146,19 +164,15 @@ app.post("/edit", async (req, res) => {
 app.post("/Update", async (req, res) => {
 
     const { description, notes, rating, update} = req.body;
-    const d = new Date();
-    console.log("body: ", req.body);
-    console.log("UPDATE: ", update);
-    console.log("Parsed: ", parseInt(update));
+    const d = new Date().toLocaleDateString('en-CA');
     await db.query(`UPDATE books SET  description = $1, notes = $2, rating = $3, note_date = $4 WHERE id = $5 `,
-        [description, notes, rating, d.toISOString(), parseInt(update)]);
+        [description, notes, rating, d, parseInt(update)]);
     
     res.redirect(`/view/${update}`);
 })
 
 app.post("/delete", async (req, res) => {
     const id = parseInt(req.body.id);
-    console.log("ID: ", id);
     await db.query("DELETE FROM books WHERE id = $1", [id]);
     res.redirect("/");
 })
